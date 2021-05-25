e145cd812962c300a5cf1e2e1acf38c752d8d899
Crowdbotics <team@crowdbotics.com>
2021-05-25 13:12:39 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/bold_voice_27373/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bold_voice_27373;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "bold_voice_27373";
+  }
+}
